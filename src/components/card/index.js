560426328f7174d5d65fe14a30453a14cd4790f1
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2020-06-11 22:19:35 -0400

    made a grid for cards

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Card = ({ word }) => (
-  <div>
-    This is a Card for "{word}"
+const Card = ({ classValue, word }) => (
+  <div className={classValue} style={{border: "2px black solid"}}>
+    <p>{word}</p>
   </div>
 );
 
--- src/components/gameboard/index.js
+++ src/components/gameboard/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
 import Card from '../card';
+import './Gameboard.css'
 
 const Gameboard = ({ words }) => (
   <div>
     <h1>This is a Gameboard</h1>
-    <pre style={{textAlign: "left", marginLeft: "30%"}}>{JSON.stringify(words, null, 2)}</pre>
-    {words.map(word => <Card word={word} />)}
+    <div className="wrapper">
+      <div className="left-sidebar">Sidebar</div>
+      <div className="gameboard">
+      {[0, 1, 2, 3, 4].map(i =>
+        <div key={i} className="card-row">
+          {words.slice(0 + i * 5, 5 + i * 5).map(word =>
+            <Card key={i + word} word={word} classValue={"card"} />
+          )}
+        </div>
+      )}
+      </div>
+      <div className="right-sidebar">Sidebar</div>
+    </div>
   </div>
 );
 
