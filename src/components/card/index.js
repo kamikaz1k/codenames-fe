0dace93c668d4fdb3d87efc57c7676e52adf9242
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2020-06-13 22:21:38 -0400

    handle double agent card styling

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -6,20 +6,24 @@ const cardStyles = {
 };
 
 const bgColourForTeam = (team) => ({
-  red: "#CD3B49",
-  blue: "#80C2FF",
-  default: "#FCECD7"
-}[team.toLowerCase()]);
+  Red: "#CD3B49",
+  Blue: "#80C2FF",
+  null: "#FCECD7",
+  doubleAgent: "black"
+}[team]);
 
-const addFontColourForTeam = (team) => (
-  team === null ? {} : { color: "white" }
+const addFontColourForBgColour = (colour) => (
+  ["#80C2FF", "#FCECD7"].includes(colour) ? {} : { color: "white" }
 );
 
 const prepareCardStyle = (word) => {
   let teamStyles = {};
+  let doubleAgent = word.isDoubleAgent ? "doubleAgent" : false;
+
   if (word.isRevealed) {
-    teamStyles.backgroundColor = bgColourForTeam(word.team || "default");
-    Object.assign(teamStyles, addFontColourForTeam(word.team));
+    const bgColour = bgColourForTeam(doubleAgent || word.team);
+    teamStyles.backgroundColor = bgColour;
+    Object.assign(teamStyles, addFontColourForBgColour(bgColour));
   }
   return Object.assign({}, cardStyles, teamStyles);
 }
