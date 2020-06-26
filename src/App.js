51a632f47a8591b3b54ea69ecdc807a52832c646
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2020-06-26 03:06:07 -0400

    added phoenix.js as npm dep and cleaned up dead code

--- src/App.js
+++ src/App.js
@@ -2,50 +2,19 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-import phx from './lib/phoenix';
+import { Socket } from 'phoenix';
 
 import Gameboard from './components/gameboard';
 
 const RED_TEAM = "red";
 const BLUE_TEAM = "blue";
+const words = [];
 
 window.inspect = (obj) => {
   console.log(obj);
   return obj;
 }
 
-// window.phx = phx;
-// window.Socket = phx.Socket;
-
-const Socket = phx.Socket;
-
-const words = [
-  // { value: "__camp", id: 1, team: BLUE_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "deer", id: 2, team: RED_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "proposal", id: 3, team: null, isDoubleAgent: false, isRevealed: false },
-  // { value: "walk", id: 4, team: BLUE_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "dip", id: 5, team: null, isDoubleAgent: false, isRevealed: false },
-  // { value: "screw", id: 6, team: BLUE_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "denial", id: 7, team: RED_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "rich", id: 8, team: null, isDoubleAgent: false, isRevealed: false },
-  // { value: "know", id: 9, team: BLUE_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "sheet", id: 10, team: BLUE_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "lose", id: 11, team: null, isDoubleAgent: false, isRevealed: false },
-  // { value: "charm", id: 12, team: RED_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "pan", id: 13, team: BLUE_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "researcher", id: 14, team: null, isDoubleAgent: true, isRevealed: false },
-  // { value: "tight", id: 15, team: null, isDoubleAgent: false, isRevealed: false },
-  // { value: "adoption", id: 16, team: RED_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "volcano", id: 17, team: BLUE_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "result", id: 18, team: BLUE_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "crack", id: 19, team: null, isDoubleAgent: false, isRevealed: false },
-  // { value: "lemon", id: 20, team: RED_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "domestic", id: 21, team: null, isDoubleAgent: false, isRevealed: false },
-  // { value: "aware", id: 22, team: null, isDoubleAgent: false, isRevealed: false },
-  // { value: "garbage", id: 23, team: RED_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "lover", id: 24, team: RED_TEAM, isDoubleAgent: false, isRevealed: false },
-  // { value: "organize", id: 25, team: null, isDoubleAgent: false, isRevealed: false }
-];
 
 class App extends React.Component {
 
@@ -68,7 +37,7 @@ class App extends React.Component {
 
     setTimeout(() => {
       this.setState(prevState => {
-        if (prevState.msg == msg) {
+        if (prevState.msg === msg) {
           return {
             ...prevState,
             msg: null
@@ -183,46 +152,7 @@ class App extends React.Component {
   }
 
   handleSelectWord = (word, activeTeam) => {
-
-    if (word.isRevealed) return;
-
-    return this.handleGameAction(word.id);
-
-    // if (word.isDoubleAgent) {
-    //   window.alert(`${activeTeam} Loses!`);
-    // }
-
-    // if (activeTeam !== word.team) {
-    //   console.log("### You picked the other team's card!", activeTeam, word);
-    // }
-
-    // let newActiveTeam = activeTeam;
-    // if (word.team && word.team !== activeTeam) {
-    //   newActiveTeam = activeTeam === RED_TEAM ? BLUE_TEAM : RED_TEAM;
-    // }
-
-    // this.setState(prevState => {
-    //   let currState = {
-    //     ...prevState,
-    //     activeTeam: newActiveTeam,
-    //     words: prevState.words.map(v => {
-    //       if (v.id === word.id) {
-    //         return {
-    //           ...word,
-    //           isRevealed: true
-    //         }
-    //       }
-
-    //       return v;
-    //     })
-    //   };
-
-    //   return window.inspect({
-    //     ...currState,
-    //     redTeamScore: currState.words.reduce((acc, curr) => (curr.isRevealed && curr.team === RED_TEAM) ? acc + 1 : acc, 0),
-    //     blueTeamScore: currState.words.reduce((acc, curr) => (curr.isRevealed && curr.team === BLUE_TEAM) ? acc + 1 : acc, 0)
-    //   });
-    // });
+    word.isRevealed || this.handleGameAction(word.id);
   }
 
   render() {
